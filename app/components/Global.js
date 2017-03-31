084488eadc37bb2ad76fea6f354e4739ebe2e159
Puttipong <zer.n3w@gmail.com>
2017-03-31 11:44:32 +0700

    first commit

---
+++ app/components/Global.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon, Button } from 'react-bootstrap';
+import Gallery from './Gallery'
+
+class Global extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            query: '',
+            items: []
+        }
+    }
+
+    search() {
+        const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q='
+        fetch(`${BASE_URL}${this.state.query}`, { method: 'GET'})
+        .then(response => response.json())
+        .then(json => {
+            let { items } = json;
+            this.setState({items})
+        });
+    }
+
+    render() {
+        return (
+            <div className="Global">
+                <h2>Book Explorer!</h2>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for a book"
+                            onChange={event => this.setState({query: event.target.value})}
+                            onKeyPress={event => {
+                                if (event.key === 'Enter'){
+                                    this.search();
+                                }
+                            }}
+                        />
+                        <InputGroup.Addon >
+                            <Glyphicon glyph="search" onClick={() => this.search()}></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <Gallery items={ this.state.items } />
+            </div>
+        )
+    }
+}
+
+export default Global;
